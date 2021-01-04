fa72ba38f35c59df7624712e7bc42568c979386a
Indomite <Indomite@qq.com>
2021-01-04 12:55:12 +0800

    ✅ 权限管理

---
+++ src/components/Roles.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/rights' }">权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- 输入框 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <el-table :data="roleList" border stripe>
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <!-- 一级权限 -->
+            <el-row :class="['bdbottom', 'vcenter', i1 === 0 ? 'bdtop':'']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
+              <el-col :span="5">
+                <el-tag>{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <el-col :span="19">
+                <!-- 二级权限 -->
+                <el-row :class="['vcenter', i2 === 0 ? '':'bdtop']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag type="success">{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <!-- 三级权限 -->
+                    <el-tag v-for="item3 in item2.children" :key="item3.id" type="warning">{{item3.authName}}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template>
+            <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+            <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+            <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      roleList: []
+    }
+  },
+  created () {
+    this.getRolesList()
+  },
+  methods: {
+    async getRolesList () {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.roleList = res.data
+      console.log(this.roleList)
+    }
+  }
+}
+
+</script>
+
+<style lang='less' scoped>
+.el-tag {
+  margin: 7px;
+}
+
+.bdtop {
+  border-top: 1px solid #eee;
+}
+
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+
+.vcenter {
+  display: flex;
+  align-items: center;
+  text-align: left;
+}
+</style>
